b83c4f0472b1d56ee3acd15a02bf28efe97f8a3e
huang <huang@HUANG-PC>
2019-01-14 00:52:37 +0800

    plus PLCcoil

---
+++ ScanCheck/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ScanCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 120);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PLCtest";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 331);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ScanCheck/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace ScanCheck
+{
+    public partial class Form1 : Form
+    {//五秒更新一次status，独占读写currentlocation，利用字符转枚举得到数字，用数字对应字符串数组的kittingbox编码，与扫码枪验证
+        //
+        
+        public enum Location
+        {
+            工位1 = 1,
+            工位2 = 2,
+            工位3 = 3,
+            工位4 = 4,
+            工位5 = 5,
+            工位6 = 6,
+            工位7 = 7,
+            工位8 = 8,
+            工位9 = 9,
+            工位10 = 10,
+        }
+
+        public string[] KittingBoxCode = { "Null", "001", "002", "003", "004", "005", "006", "007", "008", "009", "010" };
+
+        Setting info;
+        int CurrentLocation;
+        public Form1()
+        {//在外部setting文件输出或读取location的信息，看后面老板要不要
+            InitializeComponent();
+
+            // 枚举转字符串 string enumStringTwo = Enum.GetName(typeof(Color), color);//推荐
+            //枚举转数值int enumValueThree = Convert.ToInt32(color);
+            if (File.Exists("setting.xml"))
+            {
+                using (StreamReader sr = new StreamReader("setting.xml"))
+                {
+                    info = (new XmlSerializer(typeof(Setting))).Deserialize(sr) as Setting;
+                }
+            }
+            else
+            {
+                InitializeXml();
+            }
+            this.ControlBox = false;//最大化最小化关闭按钮
+            this.MaximizeBox = false;//窗口大小固定
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;//固定格式
+            this.TopMost = true;//窗口前置
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text != KittingBoxCode[CurrentLocation])
+                    MessageBox.Show("Error box", "提示"); 
+                else
+                {
+                    //放能重置PLC线圈为1的发送
+                }
+            }
+        }
+        private void InitializeXml()
+        {
+            Setting settingInit = new Setting
+            {//这里设置取status和plc的路劲
+                UserName = "admin",
+                UserPwd = "admin",
+                GUID = "xxxx-xxxx-xxxx",
+                Authority = MirEncode.Encode("admin", "admin"),
+                basicIp = "http://10.15.181.62/api/v2.0.0",
+                postIP = "plccore",
+                getIP = "missions",
+                postBody = "{mission_id=xxxx-xxxx-xxxx}",
+            };
+            info = settingInit;
+            using (StreamWriter sw = File.CreateText(@"setting.xml"))
+            {
+                (new XmlSerializer(typeof(Setting))).Serialize(sw, settingInit);
+            }
+        }
+        public class Setting
+        {
+            public string UserName { get; set; }
+            public string UserPwd { get; set; }
+            public string GUID { get; set; }
+            public string Authority { get; set; }
+            public string basicIp { get; set; }
+            public string postIP { get; set; }
+            string[] Location { get; set; }
+            public string getIP { get; set; }
+            public string postBody { get; set; }
+        }
+
+    }
+}
